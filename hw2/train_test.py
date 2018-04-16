0cf6ff166f44f07742e6d2a096405ec0d5a5db27
jongpillee <jongpillee.brian@gmail.com>
2018-04-16 21:11:31 +0900

    modi

--- hw2/train_test.py
+++ hw2/train_test.py
@@ -1,7 +1,7 @@
 # GCT634 (2018) HW2
 #
 # Apr-11-2018: initial version
-#
+# 
 # Jongpil Lee
 #
 
@@ -10,6 +10,7 @@ import sys
 import os
 import numpy as np
 import random
+import time
 
 import torch
 from torch.utils.data import DataLoader
@@ -21,6 +22,9 @@ from torch.optim import lr_scheduler
 torch.backends.cudnn.benchmark = True
 torch.backends.cudnn.enabled = True
 
+# gpu_option
+gpu_use = 0
+which_gpu = 0
 
 # options
 melBins = 128
@@ -32,7 +36,7 @@ num_epochs = 50
 num_frames = 120
 
 # A location where labels and features are located
-label_path = './gtzan/'
+label_path = '/home1/irteam/users/jongpil/data/GTZAN_split/'
 
 # read train / valid / test lists
 y_train_dict = {}
@@ -69,7 +73,7 @@ for iter in range(len(y_test_dict)):
             y_test_dict[test_list[iter]] = iter2
 
 
-mel_path = './gtzan_mel/'
+mel_path = '/home1/irteam/users/jongpil/gct634-2018/hw2/gtzan_mel/'
 
 # load data
 x_train = np.zeros((len(train_list),melBins,frames))
@@ -116,7 +120,7 @@ class gtzandata(Dataset):
         mel = self.x[index]
 
         entry = {'mel': mel, 'label': self.y[index]}
-
+        
         return entry
 
     def __len__(self):
@@ -137,7 +141,7 @@ test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True, drop_la
 class model_1DCNN(nn.Module):
     def __init__(self):
         super(model_1DCNN, self).__init__()
-
+   
         self.conv0 = nn.Sequential(
             nn.Conv1d(128, 32, kernel_size=8, stride=1, padding=0),
             nn.BatchNorm1d(32),
@@ -181,7 +185,10 @@ class model_1DCNN(nn.Module):
         return out
 
 # load model
-model = model_1DCNN()
+if gpu_use == 1:
+	model = model_1DCNN().cuda(which_gpu)
+elif gpu_use == 0:
+	model = model_1DCNN()
 
 # training
 criterion = nn.CrossEntropyLoss()
@@ -199,9 +206,13 @@ def fit(model,train_loader,valid_loader,criterion,learning_rate,num_epochs):
             audio = data['mel']
             label = data['label']
             # have to convert to an autograd.Variable type in order to keep track of the gradient...
-            audio = Variable(audio).type(torch.FloatTensor)
-            label = Variable(label).type(torch.LongTensor)
-
+            if gpu_use == 1:
+                audio = Variable(audio).type(torch.FloatTensor).cuda(which_gpu)
+                label = Variable(label).type(torch.LongTensor).cuda(which_gpu)
+            elif gpu_use == 0:
+                audio = Variable(audio).type(torch.FloatTensor)
+                label = Variable(label).type(torch.LongTensor)
+	
             optimizer.zero_grad()
             outputs = model(audio)
 
@@ -229,19 +240,23 @@ def eval(model,valid_loader,criterion):
     label_all = []
 
     for i, data in enumerate(valid_loader):
-        model.eval()
         audio = data['mel']
         label = data['label']
         # have to convert to an autograd.Variable type in order to keep track of the gradient...
-        audio = Variable(audio).type(torch.FloatTensor)
-        label = Variable(label).type(torch.LongTensor)
+        if gpu_use == 1:
+            audio = Variable(audio).type(torch.FloatTensor).cuda(which_gpu)
+            label = Variable(label).type(torch.LongTensor).cuda(which_gpu)
+        elif gpu_use == 0:
+            audio = Variable(audio).type(torch.FloatTensor)
+            label = Variable(label).type(torch.LongTensor)
+	
         outputs = model(audio)
         loss = criterion(outputs, label)
 
         eval_loss += loss.data[0]
 
-        output_all.append(outputs.data.numpy())
-        label_all.append(label.data.numpy())
+        output_all.append(outputs.data.cpu().numpy())
+        label_all.append(label.data.cpu().numpy())
 
     avg_loss = eval_loss/len(valid_loader)
     print ('Average loss: {:.4f} \n'. format(avg_loss))
@@ -251,7 +266,9 @@ def eval(model,valid_loader,criterion):
     return avg_loss, output_all, label_all
 
 # run
+start_time = time.time()
 fit(model,train_loader,valid_loader,criterion,learning_rate,num_epochs)
+print("--- %s seconds ---" % (time.time() - start_time))
 
 
 # evaluation
@@ -271,3 +288,10 @@ acc = float(len(test_list) - np.count_nonzero(comparison)) / len(test_list)
 print('Test Accuracy: {:.4f} \n'. format(acc))
 
 # TODO segmentation eval function average !!!
+
+
+
+
+
+
+
